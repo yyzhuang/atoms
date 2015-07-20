8f258cfc1ae68389a0a4436dc35c4d7a0033ff2e
yyzhuang <yanyanzhuang83@gmail.com>
2015-07-20 11:08:44 -0700

    separate if and for

--- atoms-of-confusion/atoms_of_confusion_API.py
+++ atoms-of-confusion/atoms_of_confusion_API.py
@@ -60,60 +60,85 @@ class atoms_of_confusion:
 
       
 
-  # apply the type conversion atom to the program...
+  # apply the arithmetic type conversion atom to the program...
+  # this atom occurs when two integers divide each other, e.g., 2/3=0, 3/2=1.
+  # note that if either of the operands is float, the resulr will be float: .1/2=.05
   def arithmetic_TYPE_atom(self, linenumber):
     # Validate arguments...
     assert(type(linenumber) is int or type(linenumber) is long)
     if linenumber < 0:
-      raise ValueError("TypeAtom requires a positive line number")
+      raise ValueError("ArithmeticTypeAtom requires a positive line number")
 
     if linenumber >= len(self.programtextlist):
-      raise ValueError("TypeAtom requires a line number (" + str(linenumber) 
+      raise ValueError("ArithmeticTypeAtom requires a line number (" + str(linenumber) 
             + ") less than the number of lines in the file (" + str(len(self.programtextlist)) + ")")
 
     programline = self.programtextlist[linenumber]
+    if '/' not in programline:
+      raise ValueError("ArithmeticTypeAtom requires a division operation")
 
-    # in case where "if (a == 2 + 3) {" or "else if (a == 2 + 3) {"
-    keywords = ['if', 'else', 'else if', 'for', 'while']
+    # currently the code handles simple arithmetic, no compound is handled yet
+    # in case where "if (a == 2 / 3) {", "else if (a > 2 / 3) {" or "while (i > 3/2) {"
+    keywords = ['if', 'else', 'else if', 'while']
     for keyword in keywords:
       if keyword in programline:
-        programline = re.sub(keyword, '', programline)
+        # split "if (a == 2 / 3) {" into ["if (", "a == 2 / 3", ") {"]
+        itemlist = re.split('([()])', programline)
+        statementhead, statement, statementtail = itemlist[:2], itemlist[2], itemlist[3:]    
+        break
+    else:
+        if 'for' in programline:
+          # split "for (i = 0; i < 2/3; i++) {" into "for (i = 0; ", "i < 2/", "; i++"
+          itemlist = re.split('([;])', programline)
+          statementhead, statement, statementtail = itemlist[:2], itemlist[2], itemlist[3:]
+        else:
+          # only statements like "double a = 2/3;"
+          itemlist = re.split('([;])', programline)
+          statementhead, statement, statementtail = [], itemlist[0], itemlist[1:]
 
     # assume: each line has only one such experssion, not "var1 = 1 + 2; var2 = 2 + 3;"
     comparelist = ['=', '==', '>', '<', '>=', '<=', '<>', '!=']    
     variablelist = []
 
     for operator in comparelist:
-      if operator in programline:
-        # lines like "var = 2 + 3;", "var == 2 + 3" or "var > 2 + 3;"
-        # first remove spaces and get "var==2+3"
-        programline = programline.replace(' ', '')
+      if operator in statement:
+        # lines like "var = 2 / 3", "var == 2 / 3" or "var > 2 / 3"
+        itemlist = re.split('([' + operator + ']+)', statement)
+        for item in itemlist:
+          if '/' not in item:
+            statementhead.append(item)
+          else:
+            expression = item
+            
+        expression = expression.replace(' ', '')
+        dividend, divisor = expression.split('/')
+        break
 
-        # split to get var, and 2+3. filter(None, list) removes '' from list
-        variable, expression = filter(None, re.split(operator, programline))
-        
-        # remove math operators: "[-(){}+*/;]" will not appear in variablelist
-        variablelist = filter(None, re.split("[-(){}+*/;]", expression))
+    dividendtype = type_int_or_float(dividend)
+    divisortype = type_int_or_float(divisor)
 
-    variabletype = []
-    for variable in variablelist:
-      variable = int_or_float(variable)
-      typeofvariable = type(variable)
-      if typeofvariable is str:
-        raise ValueError("TypeAtom currently do not handle dynamic types.")
-      variabletype.append(typeofvariable)
-
-    if variabletype[1:] == variabletype[:-1]:
-      # all variable types are identical
-      raise ValueError("TypeAtom requires implicit type conversion.")
+    if dividendtype is int and divisortype is int:
+      line = ''
+      for head in statementhead:
+        line = line + str(head)
+      line = line + ' (int) ' + expression
+      for tail in statementtail:
+        line = line + str(tail)
     else:
-      # some variable types are different
-      print "There is an implicit type conversion."
-      return variabletype
+      # if one of the dividend or divisor is float, the result is float
+      raise ValueError("There is no ArithmeticTypeAtom!")
+
+    self.programtextlist[linenumber] = line
+
 
     
-def int_or_float(number):
+def type_int_or_float(number):
+  # number is a string. return the type of number represents
+  # if number is '2', then return int
+  # if number is '2.0', then return float
   try:
-    return int(number)
+    convertednumber = int(number)
   except ValueError:
-    return float(number)
+    convertednumber = float(number)
+    
+  return type(convertednumber)
--- atoms-of-confusion/type_unit_test.py
+++ atoms-of-confusion/type_unit_test.py
@@ -2,13 +2,9 @@ import atoms_of_confusion_API
 
 # This test demonstrates the proper use of the atoms_of_confusion clas
 # to add the indentation atom of confusion
-print('Unit Test 1')
+print('======Unit Test 1======')
 
-# This contains the program
-# if (foo)
-#   goto fail;
-# goto fail;
-sampleprogramtext = ['if ( a == 2 + 3.0)', '  print a;']
+sampleprogramtext = ['if ( a == 2/3)', '  printf("a=%.3f", a);']
 
 # create a new instance of the atoms_of_confusion clas with the sample program text
 atoms_of_confusionClass = atoms_of_confusion_API.atoms_of_confusion(sampleprogramtext)
@@ -17,4 +13,70 @@ atoms_of_confusionClass = atoms_of_confusion_API.atoms_of_confusion(sampleprogra
 atoms_of_confusionClass.printprogram()
 
 # add the atom of confusion
-print atoms_of_confusionClass.arithmetic_TYPE_atom(0)
+try:
+  atoms_of_confusionClass.arithmetic_TYPE_atom(0)
+  atoms_of_confusionClass.printprogram()
+except ValueError:
+  print "there was an error!"
+
+
+print('\n\n======Unit Test 2======')
+
+# This contains the program
+# for (int i = 0; i < 2/3; i++) {
+#   printf("a=%.3f", a);
+# }
+sampleprogramtext = ['for (int i = 0; i < 2/3; i++) {', '  printf("a=%.3f", a);', '}']
+
+# create a new instance of the atoms_of_confusion clas with the sample program text
+atoms_of_confusionClass = atoms_of_confusion_API.atoms_of_confusion(sampleprogramtext)
+
+# print out the original program text
+atoms_of_confusionClass.printprogram()
+
+# add the atom of confusion
+try:
+  atoms_of_confusionClass.arithmetic_TYPE_atom(0)
+  atoms_of_confusionClass.printprogram()
+except ValueError:
+  print "there was an error!"
+
+
+print('\n\n======Unit Test 3======')
+
+# This contains the program
+# double a = 2/3;
+sampleprogramtext = ['double a = 2/3;']
+
+# create a new instance of the atoms_of_confusion clas with the sample program text
+atoms_of_confusionClass = atoms_of_confusion_API.atoms_of_confusion(sampleprogramtext)
+
+# print out the original program text
+atoms_of_confusionClass.printprogram()
+
+# add the atom of confusion
+try:
+  atoms_of_confusionClass.arithmetic_TYPE_atom(0)
+  atoms_of_confusionClass.printprogram()
+except ValueError as e:
+  print "there was an error!", str(e)
+
+
+print('\n\n======Unit Test 4======')
+
+# This contains the program
+# double a = 2/3;
+sampleprogramtext = ['double a = 2.5/3;']
+
+# create a new instance of the atoms_of_confusion clas with the sample program text
+atoms_of_confusionClass = atoms_of_confusion_API.atoms_of_confusion(sampleprogramtext)
+
+# print out the original program text
+atoms_of_confusionClass.printprogram()
+
+# add the atom of confusion
+try:
+  atoms_of_confusionClass.arithmetic_TYPE_atom(0)
+  atoms_of_confusionClass.printprogram()
+except ValueError as e:
+  print "there was an error!", str(e)
